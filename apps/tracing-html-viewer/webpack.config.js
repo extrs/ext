c6270709fde1a84b01df7850a1579b1a0af3aff8
Donny <kdy1997.dev@gmail.com>
2021-12-24 19:37:28 +0900

    fixup: swc

--- apps/tracing-html-viewer/webpack.config.js
+++ apps/tracing-html-viewer/webpack.config.js
@@ -6,5 +6,23 @@ module.exports = {
     entry: './src/index.ts',
     output: {
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts|\.tsx$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: "swc-loader",
+                    options: {
+                        jsc: {
+                            parser: {
+                                syntax: "typescript"
+                            }
+                        }
+                    }
+                }
+            }
+        ],
     }
 }
