ac632ba5c735eb81ac35a4da432abe367fa1de90
Donny <kdy1997.dev@gmail.com>
2021-12-24 20:06:31 +0900

    WIP

--- apps/tracing-html-viewer/src/viewer/App.tsx
+++ apps/tracing-html-viewer/src/viewer/App.tsx
@@ -5,6 +5,14 @@ export interface AppProps {
   events: TraceEvent[];
 }
 
-const App: React.FC<AppProps> = () => {};
+const App: React.FC<AppProps> = ({ events }) => {
+  return (
+    <div>
+      {events.map((event, i) => (
+        <div key={i}>{event.fields.message}</div>
+      ))}
+    </div>
+  );
+};
 
 export default App;
