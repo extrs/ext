b041660a6d6c09ba7f14b78628cd5d5b8c50d6b3
Donny <kdy1997.dev@gmail.com>
2021-12-24 16:26:05 +0900

    More test

--- crates/tracing_html/tests/fixture.rs
+++ crates/tracing_html/tests/fixture.rs
@@ -2,7 +2,7 @@ use anyhow::Error;
 use tracing_html::html_layer;
 use tracing_subscriber::prelude::*;
 
-fn init() -> Result<tracing::dispatcher::DefaultGuard, Error> {
+fn init(name: &str) -> Result<tracing::dispatcher::DefaultGuard, Error> {
     let logger = tracing_subscriber::FmtSubscriber::builder()
         .without_time()
         .with_target(false)
@@ -10,16 +10,62 @@ fn init() -> Result<tracing::dispatcher::DefaultGuard, Error> {
         .with_test_writer()
         .pretty()
         .finish()
-        .with(html_layer("simple1.html".into())?);
+        .with(html_layer(name.into())?);
 
     Ok(tracing::subscriber::set_default(logger))
 }
 
 #[test]
 fn simple_1() -> Result<(), Error> {
-    let _guard = init()?;
+    let _guard = init("simple1.html")?;
 
     tracing::info!("Hello, world!");
+
+    Ok(())
+}
+
+#[test]
+fn multiple() -> Result<(), Error> {
+    let _guard = init("multiple.html")?;
+
+    tracing::info!("Hello, world!");
+    tracing::error!("Hello, world!");
+
+    Ok(())
+}
+
+#[test]
+fn instrument() -> Result<(), Error> {
+    let _guard = init("instrument.html")?;
+
+    #[tracing::instrument]
+    fn call() {
+        tracing::info!("Hello, world!");
+    }
+
+    tracing::error!("Hello, world!");
+    call();
+
+    Ok(())
+}
+
+#[test]
+fn instrument_nested() -> Result<(), Error> {
+    let _guard = init("instrument_nested.html")?;
+
+    #[tracing::instrument]
+    fn call() {
+        tracing::info!("Hello, world!");
+        call2();
+    }
+
+    #[tracing::instrument]
+    fn call2() {
+        tracing::warn!("Hello, world!");
+    }
+
+    tracing::error!("Hello, world!");
+    call();
 
     Ok(())
 }
