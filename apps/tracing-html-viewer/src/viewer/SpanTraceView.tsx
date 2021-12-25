3d063bc2d22fd5caae524b5f6eaccee5aecdb352
Donny <kdy1997.dev@gmail.com>
2021-12-25 18:27:15 +0900

    Grid

--- apps/tracing-html-viewer/src/viewer/SpanTraceView.tsx
+++ apps/tracing-html-viewer/src/viewer/SpanTraceView.tsx
@@ -13,29 +13,64 @@ const SpanTraceView: React.FC<SpanViewProps> = ({ spanDecls, id, data }) => {
   const [expanded, setExpanded] = useState(true);
 
   return (
-    <div>
-      {id && <div>{spanDecls[id]?.metadata.name ?? `<Unknown>`}</div>}
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "75px 1fr",
+      }}
+    >
+      {id && (
+        <div
+          style={{
+            gridColumn: 1,
+            gridRow: 1,
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          {id && data.spans.length > 0 && (
+            <>{!expanded ? <ExpandMoreIcon /> : <ChevronRightIcon />}</>
+          )}
 
-      {id && spanDecls[id] && Object.keys(spanDecls[id].attrs).length > 0 && (
-        <div>
-          <span>Attributes:</span>
-
-          <div>
-            {Object.entries(spanDecls[id].attrs).map(([key, value]) => (
-              <div key={key}>
-                <span>{key}:</span>
-                <span>{value}</span>
-              </div>
-            ))}
-          </div>
+          {id && (
+            <div style={{ display: "inline" }}>
+              <b>{spanDecls[id]?.metadata.name ?? `<Unknown>`}</b>
+            </div>
+          )}
         </div>
       )}
 
-      {data.enteredAt}
-      {data.spans.length > 0 && (
-        <div style={{ display: "flex", flexDirection: "row" }}>
-          <div>{!expanded ? <ExpandMoreIcon /> : <ChevronRightIcon />}</div>
-          <div style={{ flexGrow: 1 }}>
+      <div
+        style={{
+          gridColumn: !!id ? 2 : 1,
+          gridRow: !!id ? 2 : 1,
+        }}
+      >
+        {id && spanDecls[id] && Object.keys(spanDecls[id].attrs).length > 0 && (
+          <div>
+            <span>Attributes:</span>
+
+            <div>
+              {Object.entries(spanDecls[id].attrs).map(([key, value]) => (
+                <div key={key}>
+                  <span>{key}:</span>
+                  <span>{value}</span>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+
+        <div>
+          <p>
+            Started: <span>{data.enteredAt ?? data.createdAt}</span>
+          </p>
+          <p>Ended: {data.exitedAt ?? `<Unknown>`}</p>
+        </div>
+
+        {data.spans.length > 0 && (
+          <div>
             {data.spans.map(([id, data]) => (
               <SpanTraceView
                 spanDecls={spanDecls}
@@ -44,8 +79,8 @@ const SpanTraceView: React.FC<SpanViewProps> = ({ spanDecls, id, data }) => {
               ></SpanTraceView>
             ))}
           </div>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 };
