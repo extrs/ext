9fd8a0520c7e32ad09dd1ddfb198b39eebc78924
Donny <kdy1997.dev@gmail.com>
2021-12-25 20:07:20 +0900

    Use `Accordion`

--- apps/tracing-html-viewer/src/viewer/EventView.tsx
+++ apps/tracing-html-viewer/src/viewer/EventView.tsx
@@ -1,3 +1,8 @@
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Accordion from "@mui/material/Accordion";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import Typography from "@mui/material/Typography";
 import React from "react";
 import { TraceEvent } from "../types";
 
@@ -8,13 +13,25 @@ export interface EventViewProps {
 const EventView: React.FC<EventViewProps> = ({ event }) => {
   return (
     <div>
-      {event.fields.message}
-      <div>Level {event.metadata.level}</div>
-      <div>Name: {event.metadata.name}</div>
-      <div>Target: {event.metadata.target}</div>
-      <div>File: {event.metadata.file}</div>
-      <div>Line: {event.metadata.line}</div>
-      <div>Module path: {event.metadata.modulePath}</div>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>{event.fields.message}</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            <div>Level {event.metadata.level}</div>
+            <div>Name: {event.metadata.name}</div>
+            <div>Target: {event.metadata.target}</div>
+            <div>File: {event.metadata.file}</div>
+            <div>Line: {event.metadata.line}</div>
+            <div>Module path: {event.metadata.modulePath}</div>
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
     </div>
   );
 };
