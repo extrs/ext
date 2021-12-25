cdc2874a0d7ed62f014a93ca66d07293778f738a
Donny <kdy1997.dev@gmail.com>
2021-12-25 15:04:04 +0900

    Json parsing

--- apps/tracing-html-viewer/src/index.tsx
+++ apps/tracing-html-viewer/src/index.tsx
@@ -7,16 +7,7 @@ const traceDataEl = document.getElementById("trace-data")!;
 
 const traceDataStr = traceDataEl.innerText;
 
-const events = traceDataStr
-  .split("\n")
-  .filter((v) => !!v)
-  .map((v) => {
-    try {
-      return JSON.parse(v);
-    } catch (e) {
-      throw new Error(`failed to parse '${v}'`);
-    }
-  });
+const { spanDecls, root: events } = JSON.parse(traceDataStr);
 console.log(events);
 
 ReactDOM.render(<App events={events} />, root);
