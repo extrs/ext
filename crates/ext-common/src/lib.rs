96e4ac2dbbfc306da79f6074dfb91441cf2f0e75
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-17 20:20:27 +0900

    Print logs to stderr

--- crates/ext-common/src/lib.rs
+++ crates/ext-common/src/lib.rs
@@ -4,6 +4,7 @@ pub fn init_logger() -> tracing::subscriber::DefaultGuard {
         .with_level(true)
         .with_thread_ids(false)
         .with_thread_names(false)
+        .with_writer(std::io::stderr)
         .finish();
 
     tracing::subscriber::set_default(logger)
