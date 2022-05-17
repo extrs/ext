a37e4a5324d82798f25bb37257e3ab04c1afd7c8
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-17 20:12:26 +0900

    `init_logger`

--- crates/ext-common/src/lib.rs
+++ crates/ext-common/src/lib.rs
@@ -1,1 +1,10 @@
-pub fn init_logger() {}
+pub fn init_logger() -> tracing::subscriber::DefaultGuard {
+    let logger = tracing_subscriber::fmt()
+        .with_ansi(true)
+        .with_level(true)
+        .with_thread_ids(false)
+        .with_thread_names(false)
+        .finish();
+
+    tracing::subscriber::set_default(logger)
+}
