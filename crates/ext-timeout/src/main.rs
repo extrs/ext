977747418c15d124c9fa03b29d33a5887c764e9b
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-17 20:26:52 +0900

    Doc

--- crates/ext-timeout/src/main.rs
+++ crates/ext-timeout/src/main.rs
@@ -2,8 +2,19 @@ use anyhow::Result;
 use clap::Parser;
 use ext_common::init_logger;
 
+/// Run command with a timeout.
 #[derive(Debug, Parser)]
-pub struct AppArgs {}
+#[clap(name = "timeout")]
+pub struct AppArgs {
+    /// The number of milliseconds to wait before terminating, or text like `1s`
+    /// or `1m`.
+    ///
+    /// --timeout 1s is identical to --timeout 1000
+    #[clap(long)]
+    pub timeout: String,
+
+    pub command: Vec<String>,
+}
 
 fn main() -> Result<()> {
     let _logger = init_logger();
