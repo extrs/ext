16a4bb133096849b9296548cb9d29b40ef33fa8a
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-19 04:28:48 +0900

    Some conds

--- crates/ddt/src/config/condition.rs
+++ crates/ddt/src/config/condition.rs
@@ -1,4 +1,20 @@
 use serde::Deserialize;
 
 #[derive(Debug, Clone, Deserialize)]
-pub enum Condition {}
+#[serde(untagged, rename_all = "camelCase")]
+pub enum Condition {
+    ///
+    /// ```yml
+    /// 
+    /// if:
+    ///   - cond a
+    ///   - cond b
+    /// ```
+    Or(Vec<Condition>),
+
+    /// ```yml  
+    /// if:
+    ///     - cond a && cond b
+    /// ```
+    And(Box<Condition>, Box<Condition>),
+}
