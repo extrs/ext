80f64e15ae7282bbb057147e2c3bd2964f55bf5e
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-19 04:16:47 +0900

    fixup

--- crates/ddt/src/config/mod.rs
+++ crates/ddt/src/config/mod.rs
@@ -8,18 +8,18 @@ pub mod condition;
 #[derive(Debug, Clone, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct ConfigFile {
-    pub(lib) rules: Vec<Rule>,
+    pub(crate) rules: Vec<Rule>,
 }
 
 #[derive(Debug, Clone, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Rule {
-    pub(lib) actions: Vec<RuleAction>,
+    pub(crate) actions: Vec<RuleAction>,
 }
 
 #[derive(Debug, Clone, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct RuleAction {
     #[serde(rename = "if")]
-    pub(lib) if_: Condition,
+    pub(crate) if_: Condition,
 }
