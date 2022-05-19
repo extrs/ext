d2570edee287940f31f213b6ce2dbf12fdd6ddb0
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-19 21:03:32 +0900

    Some work

--- crates/ddt/src/lib.rs
+++ crates/ddt/src/lib.rs
@@ -1,2 +1,9 @@
 pub mod config;
+pub mod config_file;
 pub mod expr;
+
+/// Manages ddt tasks in a directory and subdirectories.
+#[derive(Debug)]
+pub struct DirServer {
+    subdirectories: Vec<DirServer>,
+}
